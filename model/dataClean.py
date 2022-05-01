107acd02637fbc0890d20ca03b4ea3d429a8452e
NEDONION <nedjiachenghu@gmail.com>
2022-04-30 19:38:09 -0500

    update dataClean format

--- model/dataClean.py
+++ model/dataClean.py
@@ -10,34 +10,45 @@ def data_clean(df):
     df["total_amount"] = df["total_amount"].apply(lambda x: 10.47 if x <= 0 else x)
 
     # find the frequency of transaction for each customer
-    df2 = df.groupby(by="customer_id")[["order_number_id"]].count()
-    df2.rename(columns={"order_number_id": "frequency"}, inplace=True)
+    df_custId_orderId = df.groupby(by="customer_id")[["order_number_id"]].count()
+    df_custId_orderId.rename(columns={"order_number_id": "frequency"}, inplace=True)
 
     # merge to the whole data
-    df3 = pd.merge(df, df2, on="customer_id")
-    df4 = df.groupby(by=["customer_id", "location_name"])[["order_number_id"]].count()
-    # find a customer has parked one or more places
-    df5 = df4.groupby(by=["customer_id"]).count()
-    df5.rename(columns={"order_number_id": "number_of_locations"}, inplace=True)
-    df6 = pd.merge(df3, df5, on="customer_id")
-    df7 = df6.groupby(by="customer_id")[["minutes"]].sum()
-    df8 = df6.groupby(by="customer_id")[["total_amount"]].sum()
-    df7.rename(columns={"minutes": "total_minutes"}, inplace=True)
-    df8.rename(columns={"total_amount": "total_amount_user"}, inplace=True)
-    df9 = pd.merge(df7, df8, on="customer_id")
-    df_new = pd.merge(df6, df9, on="customer_id")
-
-    # find the most popular things for a customer
-    dfb = df_new.groupby(by=["customer_id", "formatted_source"])[
+    df_custId = pd.merge(df, df_custId_orderId, on="customer_id")
+    df_custIdAndLocName_orderId = df.groupby(by=["customer_id", "location_name"])[
         ["order_number_id"]
     ].count()
-    g = dfb.reset_index().groupby(["customer_id"])["order_number_id"].idxmax()
-    dfb = dfb.reset_index("formatted_source")
-    dfb.rename(columns={"formatted_source": "popular_formatted_source"}, inplace=True)
-    dfc = dfb.iloc[g]
+    # find a customer has parked one or more places
+    df_custIdAndLocName = df_custIdAndLocName_orderId.groupby(
+        by=["customer_id"]
+    ).count()
+    df_custIdAndLocName.rename(
+        columns={"order_number_id": "number_of_locations"}, inplace=True
+    )
 
+    df_custId_new = pd.merge(df_custId, df_custIdAndLocName, on="customer_id")
+    df_custId_mins = df_custId.groupby(by="customer_id")[["minutes"]].sum()
+    df_custId_total = df_custId.groupby(by="customer_id")[["total_amount"]].sum()
+    df_custId_mins.rename(columns={"minutes": "total_minutes"}, inplace=True)
+    df_custId_total.rename(columns={"total_amount": "total_amount_user"}, inplace=True)
+
+    df_custId_mins_total = pd.merge(df_custId_mins, df_custId_total, on="customer_id")
+    df_new = pd.merge(df_custId_new, df_custId_mins_total, on="customer_id")
+
+    # find the most popular things for a customer
+    df_orderCnt = df_new.groupby(by=["customer_id", "formatted_source"])[
+        ["order_number_id"]
+    ].count()
+    MaxIndexOfOrder = (
+        df_orderCnt.reset_index().groupby(["customer_id"])["order_number_id"].idxmax()
+    )
+    df_orderCnt = df_orderCnt.reset_index("formatted_source")
+    df_orderCnt.rename(
+        columns={"formatted_source": "popular_formatted_source"}, inplace=True
+    )
+
+    dfc = df_orderCnt.iloc[MaxIndexOfOrder]
     dfc.drop(columns=["order_number_id"], inplace=True)
-
     df_new = pd.merge(df_new, dfc, on="customer_id")
 
     dfb = df_new.groupby(by=["customer_id", "payment_method_type"])[
